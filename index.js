e3f5140037b234662f63a977d86f2e01f2e44ed6
Johnny Soong <johnnysoong@hotmail.com>
2018-05-31 17:48:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,25 +14,10 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const array = Array.prototype.slice.call(document.querySelector('#grand-node').querySelectorAll('div'));
-  let current = array;
-  let next = [];
-  while (current.length > 0) {
-    console.log(current)
-    if (current[0].children.length === 0) {
-      return current[0].innerHTML;
-    } else {
-      for (let i = 0; i < current.length; i++) {
-        next.push(current[i]);
-      }
-    }
-    current = next.shift()
-  }
- 
-  // if we haven't
-  return null;
+
+  
 }
 
-console.log(deepestChild())
+
 
 
